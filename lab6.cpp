e7bffc1f320a14356009791a9a816c2fa272cb0a
LeakySum <leakysum60@gmail.com>
2017-05-12 14:07:14 +0300

    Update lab6.cpp

--- lab6.cpp
+++ lab6.cpp
@@ -7,7 +7,7 @@ class complex {
 	double b;
 
 public:
-	void add(double a, double b)
+	complex(double a, double b)
 	{
 		this->a = a;
 		this->b = b;
@@ -20,28 +20,41 @@ public:
 		b = 0;
 	}
 
-	void sum(complex num)
+	void sum(complex num1, complex num2 )
 	{
-		a += num.a;
-		b += num.b;
+		double a1=0.0, b1=0.0;
+		cout << "sum=";
+		a1 = num1.a+ num2.a;
+		b1 += num1.b + num2.b;
+		cout << a1 << " + i*(" << b1 << ')' << endl;
 	}
 	
-	void sub(complex num)
+	void sub(complex num1, complex num2 )
 	{
-		a -= num.a;
-		b -= num.b;
+		double a1 = 0.0, b1 = 0.0;
+		cout << "sub=";
+		a1 = num1.a- num2.a ;
+		b1 = num1.b- num2.b;
+		cout << a1 << " + i*(" << b1 << ')' << endl;
 	}
-	void mul(int num)
+	void mul(complex num1, int x)
 	{
-		a *= num;
-		b *= num;
+		double a1 = 0.0, b1 = 0.0;
+		cout << "mul=";
+		
+		a1 = num1.a*x;
+		b1 = num1.b*x;
+		cout << a1 << " + i*(" << b1 << ')' << endl;
 	}
-	void div(int num)
+	void div(complex num1, int x)
 	{
-		if (num) {
-			a /= num;
-			b /= num;
+		double a1 = 0.0, b1 = 0.0;
+		if (x) {
+			a1 = num1.a/x;
+			b1 = num1.b/x;
+			cout << "div=";
 		} else cout << "You can't divide by zero !" << endl;
+		cout << a1 << " + i*(" << b1 << ')' << endl;
 	}
 	
 	void print()
@@ -53,15 +66,16 @@ public:
 
 int main()
 {
-	complex num1, num2;
-	
-	num1.add(10, 2); num1.print();
-	num2.add(4, 3);  num2.print();
-
-	num1.sum(num2); 	num1.print();
-	num1.sub(num2); 	num1.print();
-	num1.mul(4); 	num1.print();
-	num1.div(2); 	num1.print();
+	int a = 10, b = 6;
+	complex num1(a, b); num1.print();
+	a = 5, b = 4; 
+	complex num2(a, b);  num2.print();
+	cout << 4 << " " << 2 << endl;
+	num1.sum(num1,num2); 	
+	num1.sub(num1, num2);
+	num1.mul(num1, 4);
+	num1.div(num1, 2);
 	system("pause");
 }
+
 
